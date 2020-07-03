f017859fca871383dac7dc26e3917f37fd96f76f
maltekliemann <mail@maltekliemann.com>
2020-07-03 23:50:02 +0200

    Add missing license stubs

--- tests/Dummy.h
+++ tests/Dummy.h
@@ -1,3 +1,21 @@
+/* Copyright 2020 Ole Kliemann, Malte Kliemann
+ *
+ * This file is part of DrMock.
+ *
+ * DrMock is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DrMock is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrMock.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef DRMOCK_TESTS_DUMMY_H
 #define DRMOCK_TESTS_DUMMY_H
 
