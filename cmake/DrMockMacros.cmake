55914a5b65678dae30f2affd97fb1c5131af9f11
maltekliemann <mail@maltekliemann.com>
2021-05-04 22:49:07 +0200

    Reorganize macros, encapsulation, etc.

--- cmake/DrMockMacros.cmake
+++ cmake/DrMockMacros.cmake
@@ -70,174 +70,6 @@ function(drmock_test)
 endfunction()
 
 
-# drmock_get_qt5_module_include_dirs(MODULE <module> INCLUDE_DIRS <include_dirs>)
-#
-# Write list of include dirs of Qt5 module <module> to <include_dirs>.
-#
-# Example: drmock_get_qt5_module_include_dirs(Qt5::Core core_include_dirs)
-function(drmock_get_qt5_module_include_dirs)
-    cmake_parse_arguments(ARGS
-        ""
-        "MODULE;INCLUDE_DIRS"
-        ""
-        ${ARGN}
-    )
-
-    STRING(REGEX REPLACE
-        "::"
-        ""
-        module_name
-        ${ARGS_MODULE}
-    )
-    set(${ARGS_INCLUDE_DIRS} ${${module_name}_INCLUDE_DIRS} PARENT_SCOPE)
-endfunction()
-
-
-# drmock_get_qt5_module_framework_path(MODULE <module>
-#                                      FRAMEWORK_PATH <framework_path>
-#                                      [QT_PATH <qt_path>])
-#
-# Write the framework path of the Qt5 module <module> to <framework_path>.
-#
-# Requires the path to the Qt5 lib/ dir. Specify the path by setting <qt_path> or 
-# the environment variable DRMOCK_QT_PATH.
-#
-# Raises: FATAL_ERROR If path to Qt5 lib/ dir is not specified.
-function(drmock_get_qt5_module_framework_path)
-    cmake_parse_arguments(ARGS
-        ""
-        "MODULE;FRAMEWORK_PATH;QT_PATH"
-        ""
-        ${ARGN}
-    )
-
-    if (NOT ARGS_QT_PATH)
-        if (NOT DEFINED ENV{DRMOCK_QT_PATH})
-            message(FATAL_ERROR "drmock_get_qt5_module_framework_path: error: no Qt5 path defined")
-        endif()
-        set(ARGS_QT_PATH $ENV{DRMOCK_QT_PATH})
-    endif()
-
-    string(REGEX REPLACE "\\\\" "" ARGS_QT_PATH ${ARGS_QT_PATH})
-    file(TO_CMAKE_PATH ARGS_QT_PATH ${ARGS_QT_PATH})
-    file(TO_NATIVE_PATH "${ARGS_QT_PATH}/lib" framework_path)
-
-    set(${ARGS_FRAMEWORK_PATH} ${framework_path} PARENT_SCOPE)
-endfunction()
-
-
-function(drmock_remove_file_extension)
-    cmake_parse_arguments(
-        ARGS
-        ""
-        "STRING;RESULT"
-        ""
-        ${ARGN}
-    )
-    set(_result)
-    STRING(REGEX REPLACE "\\.[^.]*$" "" _result ${ARGS_STRING})
-    set(${ARGS_RESULT} ${_result} PARENT_SCOPE)
-endfunction()
-
-
-function(drmock_join_paths)
-    cmake_parse_arguments(
-        ARGS
-        ""
-        "RESULT"
-        "PATHS"
-        ${ARGN}
-    )
-
-    foreach (path ${ARGS_PATHS})
-        file(TO_CMAKE_PATH path ${path})
-        if (NOT _result)
-            set(_result ${path})
-            continue()
-        endif()
-        set(_result "${_result}/${path}")
-    endforeach()
-    file(TO_NATIVE_PATH ${_result} _result)
-
-    set(${ARGS_RESULT} ${_result} PARENT_SCOPE)
-endfunction()
-
-
-
-# drmock_path_to_output(HEADER
-#                       IFILE
-#                       MOCKFILE
-#                       OUTPUT_HEADER
-#                       OUTPUT_SOURCE)
-#
-# Compute output header and output source file names; write them to
-# `OUTPUT_HEADER` and `OUTPUT_SOURCE`, resp.
-function(drmock_path_to_output)
-    cmake_parse_arguments(
-        ARGS
-        ""
-        "HEADER;IFILE;MOCKFILE;OUTPUT_HEADER;OUTPUT_SOURCE"
-        ""
-        ${ARGN}
-    )
-    set(_output_header)
-    set(_output_source)
-
-    get_filename_component(absolute_path_to_header ${header} ABSOLUTE) # /[...]/project/[DIRS]/IExample.h
-    get_filename_component(absolute_directory_of_header ${absolute_path_to_header} DIRECTORY) #/[...]/project/[DIRS]
-    file(RELATIVE_PATH relative_path_from_source_dir_to_header ${CMAKE_CURRENT_SOURCE_DIR} ${absolute_directory_of_header})  # [DIRS]
-
-    # If `relative_path_from_source_dir_to_header` is empty, set it to equal to the current
-    # path. This will later allowing uncompilcated path joins.
-    if ("${relative_path_from_source_dir_to_header}" EQUAL "")
-        set(relative_path_from_source_dir_to_header ".")
-    endif ()
-
-    # Get the filename of `header`.
-    get_filename_component(header_filename ${header} NAME)  # IExample.h
-    # Remove the file extension.
-    drmock_remove_file_extension(STRING ${header_filename}
-                                 RESULT header_filename_without_extension)  # IExample
-    # Strip the interface cast from the file name.
-    STRING(REGEX REPLACE
-        ${ARGS_IFILE}
-        "\\1"
-        captured_expression_from_filename
-        ${header_filename_without_extension}
-    )  # Example
-    # Cast the unadorned filename onto the mock cast.
-    STRING(REGEX REPLACE
-        "\\\\1"
-        ${captured_expression_from_filename}
-        mock_filename_without_extension
-        ${ARGS_MOCKFILE}
-    )  # ExampleMock
-
-    # Get the header file's file extension.
-    string(REGEX MATCH "\\.([^.]*)$" header_file_extension ${header_filename})  # .h, .hpp
-    # Compute the mock object's header and source file names.
-    set(mock_header_filename ${mock_filename_without_extension}${header_file_extension})  # ExampleMock.h, ExampleMock.hpp
-    set(mock_source_filename ${mock_filename_without_extension}.cpp)  # ExampleMock.cpp
-    # Create a directory for the mock object's header and source files.
-    drmock_join_paths(RESULT path_to_mock_subdirectory
-                      PATHS ${CMAKE_CURRENT_BINARY_DIR}
-                            DrMock/mock
-                            ${relative_path_from_source_dir_to_header})
-    file(MAKE_DIRECTORY ${path_to_mock_subdirectory})
-    # Compute the path to the mock object's header and source files
-    # relative to cmake's current binary source directory.
-    drmock_join_paths(
-        RESULT _output_header
-        PATHS DrMock/mock ${relative_path_from_source_dir_to_header} ${mock_header_filename})
-    drmock_join_paths(
-        RESULT _output_source
-        PATHS DrMock/mock ${relative_path_from_source_dir_to_header} ${mock_source_filename})
-
-    set(${ARGS_OUTPUT_HEADER} ${_output_header} PARENT_SCOPE)
-    set(${ARGS_OUTPUT_SOURCE} ${_output_source} PARENT_SCOPE)
-endfunction()
-
-
 function(drmock_library)
     cmake_parse_arguments(
         ARGS
@@ -257,10 +89,10 @@ function(drmock_library)
     _drmock_optional_param(ARGS_GENERATOR "DrMockGenerator")
 
     # Make a directory for the mock object's header and source files.
-    drmock_join_paths(RESULT drmock_directory
-                      PATHS ${CMAKE_CURRENT_BINARY_DIR} DrMock)  # Required later.
-    drmock_join_paths(RESULT mock_directory
-                      PATHS drmock_directory mock)
+    _drmock_join_paths(RESULT drmock_directory
+                       PATHS ${CMAKE_CURRENT_BINARY_DIR} DrMock)  # Required later.
+    _drmock_join_paths(RESULT mock_directory
+                       PATHS drmock_directory mock)
     file(MAKE_DIRECTORY mock_directory)
 
     # Define a list to hold the paths of the source files.
@@ -269,11 +101,14 @@ function(drmock_library)
     # If Qt is enabled, add the Qt framework and include paths.
     foreach (module ${ARGS_QTMODULES})
         if (APPLE)
-            drmock_get_qt5_module_framework_path(MODULE module FRAMEWORK_PATH qt_framework_path)
+            _drmock_get_qt5_module_framework_path(
+                MODULE module
+                FRAMEWORK_PATH qt_framework_path
+            )
             list(APPEND ARGS_FRAMEWORKS ${qt_framework_path})
         endif()
 
-        drmock_get_qt5_module_include_dirs(MODULE ${module} INCLUDE_DIRS moduleVar)
+        _drmock_get_qt5_module_include_dirs(MODULE ${module} INCLUDE_DIRS moduleVar)
         list(APPEND ARGS_INCLUDE ${moduleVar})
 
         list(APPEND ARGS_LIBS ${module})
@@ -293,7 +128,7 @@ function(drmock_library)
         ###################################
         # Path computations.
         ###################################
-        drmock_path_to_output(
+        _drmock_path_to_output(
             IFILE ${ARGS_IFILE}
             MOCKFILE ${ARGS_MOCKFILE}
             HEADER ${header}
@@ -305,17 +140,17 @@ function(drmock_library)
         # Compute the path to the mock object's header and sourcfiles
         # relative to the working directory of the mock script (at the
         # moment, this is cmake's current binary source directory.
-        drmock_join_paths(
+        _drmock_join_paths(
             RESULT path_from_working_dir_to_output_header
             PATHS ${CMAKE_CURRENT_BINARY_DIR} ${mock_header_path})
 
-        drmock_options_from_list(
+        _drmock_options_from_list(
             OPTION "-I"
             INPUT ${ARGS_INCLUDE}
             RESULT generator_option_include_directory
         )
         if (ARGS_FRAMEWORKS)
-            drmock_options_from_list(
+            _drmock_options_from_list(
                 OPTION "-iframework"
                 INPUT ${ARGS_FRAMEWORKS}
                 RESULT generator_option_iframework
@@ -371,6 +206,176 @@ function(drmock_library)
 endfunction()
 
 
+# _drmock_get_qt5_module_include_dirs(MODULE <module> INCLUDE_DIRS <include_dirs>)
+#
+# Write list of include dirs of Qt5 module <module> to <include_dirs>.
+#
+# Example:
+#     _drmock_get_qt5_module_include_dirs(Qt5::Core core_include_dirs)
+function(_drmock_get_qt5_module_include_dirs)
+    cmake_parse_arguments(ARGS
+        ""
+        "MODULE;INCLUDE_DIRS"
+        ""
+        ${ARGN}
+    )
+
+    STRING(REGEX REPLACE
+        "::"
+        ""
+        module_name
+        ${ARGS_MODULE}
+    )
+    set(${ARGS_INCLUDE_DIRS} ${${module_name}_INCLUDE_DIRS} PARENT_SCOPE)
+endfunction()
+
+
+# _drmock_get_qt5_module_framework_path(MODULE <module>
+#                                       FRAMEWORK_PATH <framework_path>
+#                                       [QT_PATH <qt_path>])
+#
+# Write the framework path of the Qt5 module <module> to
+# <framework_path>.
+#
+# Requires the path to the Qt5 lib/ dir. Specify the path by setting
+# <qt_path> or the environment variable DRMOCK_QT_PATH.
+#
+# Raises: FATAL_ERROR If path to Qt5 lib/ dir is not specified.
+function(_drmock_get_qt5_module_framework_path)
+    cmake_parse_arguments(ARGS
+        ""
+        "MODULE;FRAMEWORK_PATH;QT_PATH"
+        ""
+        ${ARGN}
+    )
+
+    if (NOT ARGS_QT_PATH)
+        if (NOT DEFINED ENV{DRMOCK_QT_PATH})
+            message(FATAL_ERROR "_drmock_get_qt5_module_framework_path: error: no Qt5 path defined")
+        endif()
+        set(ARGS_QT_PATH $ENV{DRMOCK_QT_PATH})
+    endif()
+
+    string(REGEX REPLACE "\\\\" "" ARGS_QT_PATH ${ARGS_QT_PATH})
+    file(TO_CMAKE_PATH ARGS_QT_PATH ${ARGS_QT_PATH})
+    file(TO_NATIVE_PATH "${ARGS_QT_PATH}/lib" framework_path)
+
+    set(${ARGS_FRAMEWORK_PATH} ${framework_path} PARENT_SCOPE)
+endfunction()
+
+
+function(_drmock_remove_file_extension)
+    cmake_parse_arguments(
+        ARGS
+        ""
+        "STRING;RESULT"
+        ""
+        ${ARGN}
+    )
+    set(_result)
+    STRING(REGEX REPLACE "\\.[^.]*$" "" _result ${ARGS_STRING})
+    set(${ARGS_RESULT} ${_result} PARENT_SCOPE)
+endfunction()
+
+
+function(_drmock_join_paths)
+    cmake_parse_arguments(
+        ARGS
+        ""
+        "RESULT"
+        "PATHS"
+        ${ARGN}
+    )
+
+    foreach (path ${ARGS_PATHS})
+        file(TO_CMAKE_PATH path ${path})
+        if (NOT _result)
+            set(_result ${path})
+            continue()
+        endif()
+        set(_result "${_result}/${path}")
+    endforeach()
+    file(TO_NATIVE_PATH ${_result} _result)
+
+    set(${ARGS_RESULT} ${_result} PARENT_SCOPE)
+endfunction()
+
+
+
+# _drmock_path_to_output(HEADER
+#                        IFILE
+#                        MOCKFILE
+#                        OUTPUT_HEADER
+#                        OUTPUT_SOURCE)
+#
+# Compute output header and output source file names; write them to
+# `OUTPUT_HEADER` and `OUTPUT_SOURCE`, resp.
+function(_drmock_path_to_output)
+    cmake_parse_arguments(
+        ARGS
+        ""
+        "HEADER;IFILE;MOCKFILE;OUTPUT_HEADER;OUTPUT_SOURCE"
+        ""
+        ${ARGN}
+    )
+    set(_output_header)
+    set(_output_source)
+
+    get_filename_component(absolute_path_to_header ${header} ABSOLUTE) # /[...]/project/[DIRS]/IExample.h
+    get_filename_component(absolute_directory_of_header ${absolute_path_to_header} DIRECTORY) #/[...]/project/[DIRS]
+    file(RELATIVE_PATH relative_path_from_source_dir_to_header ${CMAKE_CURRENT_SOURCE_DIR} ${absolute_directory_of_header})  # [DIRS]
+
+    # If `relative_path_from_source_dir_to_header` is empty, set it to equal to the current
+    # path. This will later allowing uncompilcated path joins.
+    if ("${relative_path_from_source_dir_to_header}" EQUAL "")
+        set(relative_path_from_source_dir_to_header ".")
+    endif ()
+
+    # Get the filename of `header`.
+    get_filename_component(header_filename ${header} NAME)  # IExample.h
+    # Remove the file extension.
+    _drmock_remove_file_extension(STRING ${header_filename}
+                                  RESULT header_filename_without_extension)  # IExample
+    # Strip the interface cast from the file name.
+    STRING(REGEX REPLACE
+        ${ARGS_IFILE}
+        "\\1"
+        captured_expression_from_filename
+        ${header_filename_without_extension}
+    )  # Example
+    # Cast the unadorned filename onto the mock cast.
+    STRING(REGEX REPLACE
+        "\\\\1"
+        ${captured_expression_from_filename}
+        mock_filename_without_extension
+        ${ARGS_MOCKFILE}
+    )  # ExampleMock
+
+    # Get the header file's file extension.
+    string(REGEX MATCH "\\.([^.]*)$" header_file_extension ${header_filename})  # .h, .hpp
+    # Compute the mock object's header and source file names.
+    set(mock_header_filename ${mock_filename_without_extension}${header_file_extension})  # ExampleMock.h, ExampleMock.hpp
+    set(mock_source_filename ${mock_filename_without_extension}.cpp)  # ExampleMock.cpp
+    # Create a directory for the mock object's header and source files.
+    _drmock_join_paths(RESULT path_to_mock_subdirectory
+                       PATHS ${CMAKE_CURRENT_BINARY_DIR}
+                             DrMock/mock
+                             ${relative_path_from_source_dir_to_header})
+    file(MAKE_DIRECTORY ${path_to_mock_subdirectory})
+    # Compute the path to the mock object's header and source files
+    # relative to cmake's current binary source directory.
+    _drmock_join_paths(
+        RESULT _output_header
+        PATHS DrMock/mock ${relative_path_from_source_dir_to_header} ${mock_header_filename})
+    _drmock_join_paths(
+        RESULT _output_source
+        PATHS DrMock/mock ${relative_path_from_source_dir_to_header} ${mock_source_filename})
+
+    set(${ARGS_OUTPUT_HEADER} ${_output_header} PARENT_SCOPE)
+    set(${ARGS_OUTPUT_SOURCE} ${_output_source} PARENT_SCOPE)
+endfunction()
+
+
 # Check if optional parameter is defined, and replace it with default
 # value otherwise.
 function(_drmock_optional_param param default)
@@ -392,12 +397,12 @@ function(_drmock_required_param param what)
 endfunction()
 
 
-# drmock_options_from_list(OPTION <option>
-#                          INPUT <input1>;<input2>;... 
-#                          RESULT <result>)
+# _drmock_options_from_list(OPTION <option>
+#                           INPUT <input1>;<input2>;... 
+#                           RESULT <result>)
 #
 # Set `result` to "<option><input1>;<option><input2>;..."
-function(drmock_options_from_list)
+function(_drmock_options_from_list)
     cmake_parse_arguments(
         ARGS
         ""
