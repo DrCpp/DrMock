2d5c8e1b860097dc974f2e4e7bfd71f005c57bc9
maltekliemann <mail@maltekliemann.com>
2021-05-04 22:40:52 +0200

    Cleanup find_* code placement

--- cmake/DrMockMacros.cmake
+++ cmake/DrMockMacros.cmake
@@ -322,16 +322,6 @@ function(drmock_library)
             )
         endif()
 
-        # Infer libclang path.
-        find_library(
-            DRMOCK_LIBCLANG_PATH
-            NAMES clang clang-6.0 clang-7.0 clang-8.0
-            PATH_SUFFIXES lib
-            HINTS
-                /Library/Developer/CommandLineTools/usr
-                /usr/lib/llvm-7/lib
-        )
-
         ###################################
         # Calling the generator.
         ###################################
@@ -348,6 +338,16 @@ function(drmock_library)
         list(APPEND command "--std=c++${CMAKE_CXX_STANDARD}")
         list(APPEND command ${ARGS_OPTIONS})
         if (NOT DEFINED ENV{CLANG_LIBRARY_FILE})
+            # If no environment variable is available, try to find
+            # libclang via CMake.
+            find_library(
+                DRMOCK_LIBCLANG_PATH
+                NAMES clang clang-6.0 clang-7.0 clang-8.0
+                PATH_SUFFIXES lib
+                HINTS
+                    /Library/Developer/CommandLineTools/usr
+                    /usr/lib/llvm-7/lib
+            )
             list(APPEND command --clang-library-file ${DRMOCK_LIBCLANG_PATH})
         endif()
         add_custom_command(
