f4fbbed3fe1405f739913b951cc8033b6464993d
maltekliemann <mail@maltekliemann.com>
2021-06-06 14:21:45 +0200

    Add static_assert
    Unfortunately, adding a static_assert which checks the type correctness
    isn't really a good idea, as it prevents comparisons between const char*
    and std::string.

--- src/mock/detail/WrapInSharedEqual.h
+++ src/mock/detail/WrapInSharedEqual.h
@@ -6,15 +6,15 @@
 
 namespace drmock { namespace detail {
 
-// Check if `T1` is a `ICompare<T2>`.
-template<typename T1, typename T2>
+// Check if `T` is a `ICompare<Base>`.
+template<typename T, typename Base>
 struct is_compare
 {
-  static constexpr bool value = std::is_base_of_v<ICompare<T2>, T1>;
+  static constexpr bool value = std::is_base_of_v<ICompare<Base>, T>;
 };
 
-template<typename T1, typename T2>
-inline constexpr bool is_compare_v = is_compare<T1, T2>::value;
+template<typename T, typename Base>
+inline constexpr bool is_compare_v = is_compare<T, Bsae>::value;
 
 template<typename Base, typename Derived, typename T>
 std::enable_if_t<is_compare_v<T, Base>, std::shared_ptr<ICompare<Base>>>
@@ -36,11 +36,13 @@ struct WrapInSharedEqual;
 template<typename... Bases, typename... Deriveds>
 struct WrapInSharedEqual<std::tuple<Bases...>, std::tuple<Deriveds...>>
 {
+  static_assert(sizeof...(Bases) == sizeof...(Deriveds));
+
   template<typename... Ts>
   std::tuple<std::shared_ptr<ICompare<Bases>>...>
   wrap(Ts&&... pack)
   {
-    // TODO static_assert
+    static_assert(sizeof...(Ts) == sizeof...(Deriveds));
     return std::make_tuple(wrap_in_shared_equal<Bases, Deriveds>(std::forward<Ts>(pack))...);
   }
 };
