bef9bf4196b5acca8adb0cb91ecc7606c0a9fe78
maltekliemann <mail@maltekliemann.com>
2021-07-09 17:16:25 +0200

    Add test for class with no default ctor

---
+++ tests/integration/INoDefaultCtor.h
@@ -0,0 +1,37 @@
+/* Copyright 2021 Ole Kliemann, Malte Kliemann
+ *
+ * This file is part of DrMock.
+ *
+ * DrMock is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DrMock is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrMock.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef DRMOCK_TESTS_INTEGRATION_NODEFAULTCTOR_H
+#define DRMOCK_TESTS_INTEGRATION_NODEFAULTCTOR_H
+
+namespace outer { namespace inner {
+
+class INoDefaultCtor
+{
+public:
+  INoDefaultCtor() = delete;
+  INoDefaultCtor(int x)
+  {
+    (void)x;
+  }
+  virtual void f() {}
+};
+
+}} // namespace outer::inner
+
+#endif /* DRMOCK_TESTS_INTEGRATION_NODEFAULTCTOR_H */
